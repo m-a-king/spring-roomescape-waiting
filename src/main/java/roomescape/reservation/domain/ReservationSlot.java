a3078aa496b498b04e571ad32a34b29bb5d34434
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-05-26 15:34:25 +0900

    feat: slot null 검증 추가

--- src/main/java/roomescape/reservation/domain/ReservationSlot.java
+++ src/main/java/roomescape/reservation/domain/ReservationSlot.java
@@ -5,11 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
+import lombok.experimental.FieldNameConstants;
+import roomescape.common.domain.DomainTerm;
+import roomescape.common.validate.Validator;
 import roomescape.theme.domain.Theme;
 import roomescape.timeslot.domain.ReservationTime;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @EqualsAndHashCode
+@FieldNameConstants(level = AccessLevel.PRIVATE)
 @Getter
 @ToString
 public class ReservationSlot {
@@ -19,6 +23,14 @@ public class ReservationSlot {
     private final Theme theme;
 
     public static ReservationSlot of(final ReservationDate date, final ReservationTime time, final Theme theme) {
+        validate(date, time, theme);
         return new ReservationSlot(date, time, theme);
+    }
+
+    private static void validate(final ReservationDate date, final ReservationTime time, final Theme theme) {
+        Validator.of(ReservationSlot.class)
+                .validateNotNull(Fields.date, date, DomainTerm.RESERVATION_DATE.label())
+                .validateNotNull(Fields.time, time, DomainTerm.RESERVATION_TIME.label())
+                .validateNotNull(Fields.theme, theme, DomainTerm.THEME.label());
     }
 }
